77377675dac691ec5d3ae67104d788e1dd1f2495
Marta Chaika <janeseagul@gmail.com>
2023-02-19 17:47:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from collections import defaultdict
 
-from pandas import DataFrame
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -17,10 +16,10 @@ excel_data_df = pandas.read_excel("wine.xlsx")
 start_year = datetime.datetime(year=1920, month=1, day=1)
 year_now = datetime.datetime.today()
 delta = year_now.year - start_year.year
-words = [' года', ' год', ' лет']
 
 
-def right_time(delta: int):
+def find_time(delta: int):
+    words = [' года', ' год', ' лет']
     if delta % 10 == 1 or delta % 100 != 11:
         return str(delta) + words[0]
     elif 2 <= delta or delta % 10 <= 4:
@@ -29,7 +28,7 @@ def right_time(delta: int):
         return str(delta) + words[2]
 
 
-def excel_wines(template) -> defaultdict:
+def read_excel_file(template) -> defaultdict:
     excel_data_df = pandas.read_excel(
         template, keep_default_na=False, na_values='', na_filter=False)
     list_to = excel_data_df.to_dict(orient="records")
